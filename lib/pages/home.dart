9c5c0282146f0147ce4110576204c318031c300d
David Saldarriaga <davidsaldarriaga.pardo@gmail.com>
2021-04-08 11:53:22 -0500

    CIN

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Paginita principal'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Paginita principal'),
+        centerTitle: true,
+      ),
     );
   }
 }
